72193e2b88a61c02756c59531b6aea3acd3a8fbe
xXHighTideXx <84487860+TheHighTide@users.noreply.github.com>
2026-04-27 22:08:51 -0600

    Modify LiveMixinPatch to include invincibility check
    Added a check for invincibility in the LiveMixin patch.

--- patches/LiveMixinPatch.cs
+++ patches/LiveMixinPatch.cs
@@ -9,7 +9,7 @@ namespace HighTide.Modding.SN.SlightlyNerfedCreatures.Patches
         [HarmonyPostfix]
         private static void Start_Postfix(LiveMixin __instance)
         {
-            if (!__instance.player) {
+            if (!__instance.player && !__instance.invincible) {
                 __instance.data.maxHealth = 1;
                 __instance.health = 1;
             }
